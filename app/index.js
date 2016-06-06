3d29d7bbf7af3ef757c49c87fcf8573c676b7f66
mijdavis2 <mdavisinsc@gmail.com>
2016-06-05 18:54:53 -0700

    :art: Use more appropriate template filenames

--- app/index.js
+++ app/index.js
@@ -31,13 +31,49 @@ module.exports = generators.Base.extend({
         }
     );
     this.fs.copyTpl(
-        this.templatePath('.*'),
-        this.destinationPath(),
-        {
-          appName: this.namespace,
-          appTitle: _.startCase(this.namespace),
-          userName: this.username
-        }
-  );
+      this.templatePath('my_app/*'),
+      this.destinationPath(this.namespace.replace("-", "_")),
+      {
+        appName: this.namespace,
+        appTitle: _.startCase(this.namespace),
+        userName: this.username
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('tests/*'),
+      this.destinationPath('tests'),
+      {
+        app_name: this.namespace,
+        appTitle: _.startCase(this.namespace),
+        userName: this.username
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('dotfiles/_travis.yml'),
+      this.destinationPath('.travis.yml'),
+      {
+        appName: this.namespace,
+        appTitle: _.startCase(this.namespace),
+        userName: this.username
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('dotfiles/_coveragerc'),
+      this.destinationPath('.coveragerc'),
+      {
+        appName: this.namespace,
+        appTitle: _.startCase(this.namespace),
+        userName: this.username
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('dotfiles/_gitignore'),
+      this.destinationPath('.gitignore'),
+      {
+        appName: this.namespace,
+        appTitle: _.startCase(this.namespace),
+        userName: this.username
+      }
+    );
   }
 });
